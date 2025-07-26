ab6d840261773f596be4e49628142f006520c4c9
Smit8222 <smitkhobragade664@gmail.com>
2025-07-26 21:41:27 +0530

    .

---
+++ functions.py
@@ -0,0 +1,16 @@
+def culc_sum(a, b):
+    sum  = a+b
+    print(sum)
+    return sum
+
+culc_sum(10, 10)
+
+culc_sum(10, 11)
+
+
+#function definition
+def cal_sum(a, b): #parameter
+    return a + b
+
+sum = cal_sum(1, 2) #function call , argument
+print(sum)
